2b0eeb46d28868568fe2100afbc0c0b83e59041c
Pierre JdlF <pierre.jdlf@gmail.com>
2014-02-09 22:31:10 +0100

    placed homepage

--- content/themes/casper/assets/js/index.js
+++ content/themes/casper/assets/js/index.js
@@ -13,6 +13,30 @@
 
         //$("#blog-logo").prependTo("#site-head-content");
 
+        console.log("loaded.");
+        
+        // i18n
+        $(".langselect button").on("click", function(e) {
+            var lang = $(e.target).data("lang");
+            console.log("switching to: "+lang);
+            $("[data-localize]").localize("emi", { language:lang });
+        });
+
+        // smooth links to sections
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+
     });
 
 }(jQuery));
