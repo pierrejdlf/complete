d015a546fa469ee9ba235d8d12d68b110f1fd956
Pierre JdlF <pierre.jdlf@gmail.com>
2014-02-15 11:43:39 +0100

    quasi

--- content/themes/casper/assets/js/locales.js
+++ content/themes/casper/assets/js/locales.js
@@ -8,18 +8,59 @@ langs['en'] = {
 	},
 
 	"title": "Europe in a word",
-	"subtitle":  "100 days to tell your vision of Europe",
+	"subtitle":  "100 days to film your Europe in a word",
 
-	"conditions": "prout",
-	"about" : "prout",
+	"call": {
+		"title": "Call for films",
+		"first": "<i> Europe in a word </ i> is a participatory media project that invites amateurs and professional filmmakers to tell their Europe in a word and a movie.",
+        "second": "<i> Europe in a word </ i> wants to propose a visual primer of the multifaceted reality of Europe today.",
+    },
+    "form": {
+    	"title": "Register your film !",
+    },
+
+	"conditions": ""+
+		"<li><h3>un film, un mot</h3>"+
+		"choose a word that you associate with Europe and make your movie around it. "+
+		"</li>"+
+		"<li><h3>forme</h3>"+
+		"no limits:  it can be a feature-fiction-horror-film, a video portrait, a documentary essay, a small fiction, a photographic collage, ... whatever you want..."+
+		"</li>"+
+		"<li><h3>durée</h3>"+
+		" the minimum duration is 1 minute <br/>"+
+        " the maximum duration is 10 minutes"+
+		"</li>"+
+		"<li><h3>subtitles</h3>"+
+		"your movie has to be subtitled in english"+
+		"</li>"+
+		"<li><h3>inscriptions deadline</h3>"+
+		"Please fill the form and send us your movie before<b>may 26th 2014</b>"+
+		"</li>"+
+		"<li><h3>awards</h3>"+
+		"The names of the winners will be given on may 31st 2014:<br/>"+
+		"• jury award: 1000 €<br/>"+
+		"• public award: 500 €"+
+		"</li>",
+
+	"about": ""+
+        "<p class='lead'>"+
+        "<b>Un peu plus tôt</b>   is a group of a dozen young writers, filmmakers, designers, engineers from Europe."+
+        "</p>"+
+        "<p class='lead'>"+
+        "In 2014, with the project <b> Europe Moving Image </ b>, the group wants to create links between european artists(amateur or professional) who use the image as a language. The call for films <b> Europe in a word </ b> is the first step in a longer reflection on the question of Europe and its representations. "+ 
+         "</ p>" + 
+         "class='lead'> <p" + 
+         "The project is supported by the PEJA <b> program </ b> of the European Union." + 
+         "On the other hand we thank Béatrice <i> Durey </ i> and the association Nisi Masa <i> </ i> for their support and advice." + 
+         "</ p>"
 
 };
-
 langs['fr'] = {
 	"menu": {
 		"europe": "europe in a word",
 		"conditions": "conditions",
-		"about": "à propos",
+		"about": "about",
+		
 	},
 
 	"title": "l'Europe en un mot",
@@ -29,6 +70,10 @@ langs['fr'] = {
 		"title": "Appel à films",
 		"first": "<i>Europe in a word</i> est un projet multimédia participatif qui invite les cinéastes amateurs et professionnels à raconter leur Europe en un mot et en un film.",
 		"second": "<i>Europe in a word</i> proposera un abécédaire visuel  de la réalité multiforme qu’est l’Europe d’aujourd’hui.",
+	},
+
+	"form": {
+		"title":"Envoyez-nous votre film !",
 	},
 
 	"conditions": ""+
@@ -52,6 +97,10 @@ langs['fr'] = {
 		"au 31 mai seront décernés:<br/>"+
 		"• le prix du jury: 1000 €<br/>"+
 		"• le prix du public: 500 €"+
+		"</li>"+
+		"<li><h3>réglement</h3>"+
+		"le réglement complet est disponible "+
+		"<a href='/assets/data/europe_moving_image.pdf' target='_new'>ici</a>"+
 		"</li>",
 
 	"about": ""+
@@ -65,4 +114,56 @@ langs['fr'] = {
         "Le projet est soutenu par le programme <b>PEJA</b> de l'Union Européenne."+
         "D'autre part nous remercions <i>Béatrice Durey</i> et l'association <i>Nisi Masa</i> pour leur soutien et leurs conseils."+
         "</p>"
+};
+
+langs['es'] = {
+	"menu": {
+		"europe": "europe in a word",
+		"conditions": "condiciones",
+		"about": "acerca de",
+	},
+
+	"title": "Europe in a word",
+	"subtitle":  "&00 días para filmar tu Europa en una palabra",
+
+	"call": {
+		"title": "Convocatoria",
+		"first": "<i> Europe in a word </i> es un proyecto participativo que invita a los cineastas aficionados y profesionales a contar su Europa en una palabra y una pelicula.",
+        "second": "<i> Europe in a word </i> quiere proponer una cartilla visual de la realidad multifacética de la Europa de hoy.",
+	},
+
+	"conditions": ""+
+		"<li><h3>una peli, una palabra</h3>"+
+		"elige una palabra que asocias con Europa y contruye tu peli alrededor de esta asociación. "+
+		"</li>"+
+		"<li><h3>forme</h3>"+
+		"sin restricciones: puede ser una ficción, un ensayo documental, un collage fotográfico, ... lo que quieras ..."+
+		"</li>"+
+		"<li><h3>durée</h3>"+
+		" La duración minima es de 1 minuto<br/>"+
+        " La duración maxima es de 10 minutos"+
+		"</li>"+
+		"<li><h3>subtitles</h3>"+
+		"Tu peli tiene que estar subtitulada en inglés"+
+		"</li>"+
+		"<li><h3>fecha límite de inscripción</h3>"+
+		"Por favor, rellena el formulario y envíanos tu película antes del <b> 26 de mayo 2014 </b>" +
+		"</li>"+
+		"<li><h3>premios</h3>"+
+		" Los premios serán anunciados el 31 de mayo 2014: <br/> " +
+        " • Premio del jurado : 1.000 € <br/> " +
+        " • Premio del público : 500 € "+
+        " </li>",
+
+		"about": ""+
+        "<p class='lead'>"+
+        "<b>Un peu plus tôt</b> es un grupo constituido de una docena de jóvenes escritores , cineastas, diseñadores, ingenieros de Europa."+
+        "</p>"+
+        "<p class='lead'>"+
+        "En 2014, con el proyecto <b> Europe Moving Image </b>, el grupo quiere crear vinculos entre artistas europeos (aficionados o professionales) que utilizan la imagen como lenguaje. La convocatoria de películas <b> Europa en una palabra < / b > es el primer paso en una reflexión más larga sobre la cuestión de Europa y de sus representaciones."+ 
+         "</p>" + 
+         "class='lead'> <p" + 
+         "El proyecto es apoyado por el programa <b> PEJA </b> de la Unión Europea. "  + 
+         "Damos las Gracias a Béatrice <i>Durey</i> y la asociación <i>Nisi Masa</i> por su apoyo y consejos." + 
+         "</p>",
 };
